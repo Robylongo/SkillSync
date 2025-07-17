df34ac280bc51977af24a34fb2982a1aa361c838
ril33 <ril33@cornell.edu>
2025-07-17 13:21:21 -0400

    Added encryption when storing the access token and modified access_token to be private. Added serialize function

--- app/models.py
+++ app/models.py
@@ -1,4 +1,12 @@
+import os
+from cryptography.fernet import Fernet
+from sqlalchemy.ext.hybrid import hybrid_property
+from dotenv import load_dotenv
+
 from . import db
+
+load_dotenv()
+fernet = Fernet(os.getenv("ENCRYPTION_KEY"))
 
 class User(db.Model):
     """
@@ -8,10 +16,27 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    access_token = db.Columnd(db.String(64), nullable = False, unique = True)
+    _access_token = db.Column("access_token", db.LargeBinary)
     github_username = db.Column(db.String(64), nullable = False, unique=True)
     resume_uploaded = db.Column(db.Boolean, default=False)
 
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "github_username": self.github_username,
+            "resume_uploaded": self.resume_uploaded
+        }
+    
+    @hybrid_property
+    def access_token(self):
+        if self._access_token:
+            return fernet.decrypt(self._access_token).decode()
+        return None
+    
+    @access_token.setter
+    def access_token(self, token_plaintext):
+        self._access_token = fernet.encrypt(token_plaintext.encode())
