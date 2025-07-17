c187f8fa09bfce67c926fc92bc6b65e71d843d9c
ril33 <ril33@cornell.edu>
2025-07-17 18:41:05 -0400

    Refactor github_handler to accept parameters and handle API response for user repositories

--- app/github_handler.py
+++ app/github_handler.py
@@ -1,20 +1,34 @@
 from . import db
 from .models import User, Repository
+import requests
 
-def github_handler():
-    username = "idk"
+def github_handler(username, access_token):
     # API Call for ALL repos
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Accept": "application/vnd.github+json"
+    }
+    response = requests.get("https://api.github.com/user/repos", headers=headers)
+
     user = User.query.filter_by(github_username = username).first()
-    repos = []
+
+    if response.status_code != 200 or not user:
+        return
+    repos = response.json()
     for repo in repos:
-        repo_name = "idk"
-        repo_description = "idk"
+        fork = repo.get("fork")
+        if fork:
+            continue
+        repo_name = repo.get("name")
+        repo_description = repo.get("description")
         repo = Repository(user_id = user.id, 
                           name = repo_name, 
                           languages = get_languages(repo), 
                           description = repo_description, 
                           commit_summary = make_commit_summary(repo))
-    return None
+        db.session.add(repo)
+        db.session.commit()
+    db.session.commit()
 
 
 def get_languages(repo):
