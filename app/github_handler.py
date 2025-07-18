9b2a0a0a65d555de9523a8ff40a484185e4e237a
ril33 <ril33@cornell.edu>
2025-07-18 13:50:10 -0400

    Implemented the get_languages method to fetch all the meaningful languages used in a users repo. Began implementing make_commit_summary

--- app/github_handler.py
+++ app/github_handler.py
@@ -23,17 +23,59 @@ def github_handler(username, access_token):
         repo_description = repo.get("description")
         repo = Repository(user_id = user.id, 
                           name = repo_name, 
-                          languages = get_languages(repo), 
+                          languages = get_languages(username, repo_name, access_token), 
                           description = repo_description, 
-                          commit_summary = make_commit_summary(repo))
+                          commit_summary = make_commit_summary(username, repo_name, access_token))
         db.session.add(repo)
-        db.session.commit()
     db.session.commit()
 
 
-def get_languages(repo):
-    return None
+def get_languages(username, repo_name, access_token):
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Accept": "application/vnd.github+json"
+    }
+    response = requests.get(f"https://api.github.com/repos/{username}/{repo_name}/languages", headers=headers)
 
-def make_commit_summary(repo):
-    return None
+    if response.status_code != 200:
+        return
+    
+    languages = response.json()
+    total = languages.sum()
+    main_languages = {
+        language: byte
+        for language, byte in languages.items()
+        if byte/total >= 0.05
+    }
+    lang_string = ", ".join(main_languages.keys())
+    return lang_string
 
+def make_commit_summary(username, repo_name, access_token):
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Accept": "application/vnd.github+json"
+    }
+
+    url = f"https://api.github.com/repos/{username}/{repo_name}/commits"
+    params = {"per_page": 100}
+    commits = []
+
+    while url:
+        response = requests.get(url, headers=headers, params=params)
+
+        if response.status_code != 200:
+            return ""
+
+        data = response.json()
+        for commit in data:
+            message = commit.get("commit", {}).get("message")
+            if message:
+                commits.append(message)
+
+        if "next" in response.links:
+            url = response.links["next"]["url"]
+            params = None
+        else:
+            break
+
+    # we have commits as a full list of commits
